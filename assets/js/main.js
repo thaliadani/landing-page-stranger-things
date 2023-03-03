f25813c462badffaf2382e370b7cd0d1ab4e4139
thaliadani <thaliadani2@gmail.com>
2023-03-03 10:55:43 -0300

    modified:   assets/js/main.js 	modified:   index.html

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+const txtName= document.getElementById('txtName')
+const txtEmail= document.getElementById('txtName')
+const txtLevel= document.getElementById('txtName')
+const txtCharacter= document.getElementById('txtName')
+const btnSubscribe= document.getElementById('txtName')
+
