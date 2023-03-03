7e912087f60c891d7b5ec6f40705257f6cb926a9
thaliadani <thaliadani2@gmail.com>
2023-03-03 12:31:54 -0300

    modified:   assets/css/styles.css 	new file:   assets/js/firebase/app.js 	new file:   assets/js/firebase/hellfire-clube.js 	modified:   assets/js/main.js 	modified:   index.html

---
+++ assets/js/firebase/app.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+apiKey: "AIzaSyAeWOxnrCQ8T6dD8Y-aFCOB8wgfclOVASs",
+authDomain: "landing-page-stranger-th-62ea3.firebaseapp.com",
+projectId: "landing-page-stranger-th-62ea3",
+storageBucket: "landing-page-stranger-th-62ea3.appspot.com",
+messagingSenderId: "890682209302",
+appId: "1:890682209302:web:1915a7792af9fef4c87137"
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+
+export default app
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,22 @@
+import { subscribeToHellfireClub } from './firebase/hellfire-clube.js'
+
+
 const txtName= document.getElementById('txtName')
-const txtEmail= document.getElementById('txtName')
-const txtLevel= document.getElementById('txtName')
-const txtCharacter= document.getElementById('txtName')
-const btnSubscribe= document.getElementById('txtName')
+const txtEmail= document.getElementById('txtEmail')
+const txtLevel= document.getElementById('txtLevel')
+const txtCharacter= document.getElementById('txtCharacter')
+const btnSubscribe= document.getElementById('btnSubscribe')
+
+
+btnSubscribe.addEventListener('click', async () =>{
+    const subscription={
+        name: txtName.value,
+        email: txtEmail.value,
+        level: txtLevel.value,
+        character: txtCharacter.value
+    }
+
+    const subscriptionId = subscribeToHellfireClub(subscription)
+})
+
 
